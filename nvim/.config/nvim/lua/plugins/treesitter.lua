133db28806254173fa53928e05cbc52d121c5871
Kristofer Andersen <kristoferatli@gmail.com>
2026-09-23 11:25:01 +0200

    ast navigate large funcs

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -46,6 +46,25 @@ return {
 	},
 
 	{
+		"nvim-treesitter/nvim-treesitter-context",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		event = { "BufReadPost", "BufNewFile" },
+		opts = {
+			max_lines = 3,
+			multiline_threshold = 1,
+		},
+		keys = {
+			{
+				"[x",
+				function()
+					require("treesitter-context").go_to_context(vim.v.count1)
+				end,
+				desc = "Jump to context",
+			},
+		},
+	},
+
+	{
 		"nvim-treesitter/nvim-treesitter-textobjects",
 		branch = "main",
 		dependencies = { "nvim-treesitter/nvim-treesitter" },
